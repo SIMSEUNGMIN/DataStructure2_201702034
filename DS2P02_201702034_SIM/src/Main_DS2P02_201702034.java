2f0253c78fc43e5cccf1c47afcff91121d6c4a6b
SIMSEUNGMIN <ssm4591@naver.com>
2019-01-11 02:48:30 +0900

    2nd assignment

---
+++ DS2P02_201702034_SIM/src/Main_DS2P02_201702034.java
@@ -0,0 +1,10 @@
+
+public class Main_DS2P02_201702034 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AppController appController = new AppController();
+		appController.run();
+	}
+
+}
